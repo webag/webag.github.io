94540ce89392f619510fe133780dd6d01d31ce16
Андрей <andreiduffy@gmail.com>
2015-08-26 16:43:03 +0300

    cleaned

--- rett/js/assets.js
+++
@@ -1,5 +0,0 @@
-//@prepros-prepend ../assets/jquery-2.1.4.min.js
-//@prepros-prepend ../assets/jquery-migrate-1.2.1.js
-//@prepros-append ../assets/agmodal/agmodal.js
-//@prepros-append ../assets/owl/owl.carousel.min.js
-//@prepros-append ../assets/drop.js
